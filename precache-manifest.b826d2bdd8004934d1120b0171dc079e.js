52ea12e142ff1653ad5c54cd0e9f7fa960a48f2d
Himanshu Srivastava <himsriva2@publicisgroupe.net>
2020-02-24 14:59:22 +0530

    Updates

---
+++ precache-manifest.b826d2bdd8004934d1120b0171dc079e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ee4e37229e2516c2915b196afaf128e",
+    "url": "/hackernews/index.html"
+  },
+  {
+    "revision": "9080085b145c3f17ec60",
+    "url": "/hackernews/static/css/main.e8f83c64.chunk.css"
+  },
+  {
+    "revision": "751a0b8929cad27c8c5a",
+    "url": "/hackernews/static/js/2.35a2380e.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/hackernews/static/js/2.35a2380e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9080085b145c3f17ec60",
+    "url": "/hackernews/static/js/main.286e49fe.chunk.js"
+  },
+  {
+    "revision": "e3bc1b5ebe7e2ffd34d6",
+    "url": "/hackernews/static/js/runtime-main.eaca6213.js"
+  }
+]);
